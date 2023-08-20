86f1f026095d4a31a861b66b29a6eb12053a3000
Alexey Sukhov <a.sukhov93@gmail.com>
2023-08-20 18:50:32 +0300

    Put Object implemented

--- storage-api/selectel-api.ts
+++ storage-api/selectel-api.ts
@@ -1,5 +1,5 @@
-import { S3Client, S3ClientConfig, GetObjectCommand } from '@aws-sdk/client-s3';
-import { StorageApi, StorageObject } from '../types';
+import { S3Client, S3ClientConfig, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { StorageApi, StorageObject, PutResult } from '../types';
 
 const client = new S3Client({
     credentials: {
@@ -12,10 +12,12 @@ const client = new S3Client({
     apiVersion: 'latest'
 } as S3ClientConfig);
 
+const BUCKET_NAME = process.env.SELECTEL_MEDIA_BUCKET;
+
 export class SelectelStorageApi implements StorageApi {
     async getObject(uri: string): Promise<StorageObject> {
         const getObjectCommand = new GetObjectCommand({
-            Bucket: 'MediaStorage',
+            Bucket: BUCKET_NAME,
             Key: uri,
         });
         const result = await client.send(getObjectCommand);
@@ -26,6 +28,26 @@ export class SelectelStorageApi implements StorageApi {
             stream: result.Body.transformToWebStream(),
             contentLength: result.ContentLength as number
         };   
+    }
+
+    async putObject(stream: ReadableStream, uri: string): Promise<PutResult> {
+        const putObjectCommand = new PutObjectCommand({
+            Bucket: BUCKET_NAME,
+            Key: uri,
+            Body: stream
+        });
+
+        try {
+            await client.send(putObjectCommand);
+            return {
+                status: 'OK'
+            };
+        } catch (error) {
+            return {
+                status: 'FAILED',
+                error: error
+            };
+        }
     }
 
 }
--- types.ts
+++ types.ts
@@ -3,12 +3,17 @@ export enum ServiceProvider {
     Selectel = 'selectel'
 }
 
-
 export interface StorageApi {
     getObject(uri: string): Promise<StorageObject>
+    putObject(stream: ReadableStream, uri: string): Promise<PutResult>;
 }
 
 export type StorageObject = {
     stream: ReadableStream<Buffer>,
     contentLength: number
 }
+
+export type PutResult = {
+    status: 'OK' | 'FAILED',
+    error?: Error
+};

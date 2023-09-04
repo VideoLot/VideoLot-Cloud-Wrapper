014e6c820a215abc1ff25daab3e168723ac0c423
Alexey Sukhov <a.sukhov93@gmail.com>
2023-09-04 22:18:51 +0300

    rename

--- types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServiceProvider = void 0;
-var ServiceProvider;
-(function (ServiceProvider) {
-    ServiceProvider["Local"] = "local";
-    ServiceProvider["Selectel"] = "selectel";
-})(ServiceProvider || (exports.ServiceProvider = ServiceProvider = {}));
